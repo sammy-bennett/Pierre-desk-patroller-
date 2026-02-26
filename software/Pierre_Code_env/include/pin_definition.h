8c5cf23fdd1bc18ac6cf8eb161325579577e8d2b
sammy-bennett <samuel.bennettt@gmail.com>
2026-02-26 16:36:47 +0000

    added wifi

--- software/Pierre_Code_env/include/pin_definition.h
+++ software/Pierre_Code_env/include/pin_definition.h
@@ -2,3 +2,6 @@
 
 const int I2C_SDA = 21; //I2C DATA D21
 const int I2C_SCL = 22; //I2C clock D22
+
+constexpr const char* SSID = "VM2857891"; 
+constexpr const char* wifi_pw = "vkKspb9p5cwz";  //constexpr to stop linker errors if includede in other .cpp files
--- software/Pierre_Code_env/src/main.cpp
+++ software/Pierre_Code_env/src/main.cpp
@@ -1,25 +1,51 @@
 #include <Arduino.h>
 #include <Wire.h>
+#include <WiFi.h>
 #include <memory>
 #include <Face.h> //esp32_eyes
 
 #include <pin_definition.h>
 
 
+bool wifi_connected;
+
 
 void setup() {
   Serial.begin(115200);
   
+//  highlevel set up for I2C  //
   while(!Wire.begin(I2C_SDA,I2C_SCL)){
     Serial.println("Cannot start I2C communication.. trying again....");
-    delay(500);
+    delay(300);
+  }
+  Serial.println("I2C initiated!");
+  
+  //  wifi set up //
+  WiFi.mode(WIFI_STA); //standard mode, like a pc (optininal)
+  Serial.println("connecting to wifi...");
+  delay(500);
+  
+  unsigned long cur_time, start_time = millis();
+  WiFi.begin(SSID,wifi_pw);
+  //wait 5 seconds but only id its not connected
+  while(WiFi.status() != WL_CONNECTED){
+    cur_time = millis();
+    if(cur_time - start_time >= 5000){ //5seconds 
+      break;
+    }
+    delay(100);
   }
 
+  wifi_connected = (WiFi.status() == WL_CONNECTED);
+  if(wifi_connected){
+    Serial.println("WIFI established!");
+    Serial.print("Local ESP32 IP: ");
+    Serial.println(WiFi.localIP());}
+  else 
+    Serial.println("WIFI NOT established!.. timeout reached");
 }
 
 void loop() {
 
 
 }
-
-
