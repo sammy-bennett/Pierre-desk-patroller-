1d7b96a9b9c2f31eb933e70a9e5157a77a158b94
sammy-bennett <samuel.bennettt@gmail.com>
2026-03-03 18:19:41 +0000

    modified:   software/Pierre_Code_env/include/pin_definition.h

--- software/Pierre_Code_env/include/pin_definition.h
+++ software/Pierre_Code_env/include/pin_definition.h
@@ -5,3 +5,4 @@ const int I2C_SCL = 22; //I2C clock D22
 
 constexpr const char* SSID = "VM2857891"; 
 constexpr const char* wifi_pw = "fkKspb9p5cwz";  //constexpr to stop linker errors if includede in other .cpp files
+constexpr const char* api_Weather = "043dd26ff6406743ef4c8a9ce1c1a799";
